78dd701eb08e7ba766eb533be816b127078bee73
Curtis Fenner <cwfenner@umich.edu>
2016-09-03 23:59:05 -0400

    Preprocess trigrams in cpp.lua

--- cpp.lua
+++ cpp.lua
@@ -1,7 +1,20 @@
+local trigraphs = {
+	["??="] = "#",
+	["??("] = "[",
+	["??/"] = "\\",
+	["??)"] = "]",
+	["??'"] = "^",
+	["??<"] = "{",
+	["??!"] = "|",
+	["??>"] = "}",
+	["??-"] = "~",
+}
+
 function stripComments(src)
 	src = src .. "\n"
 	local stopComment = nil
 	local out = ""
+	-- XXX: this does not handle quotes correctly.
 	while #src > 0 do
 		local text1, comment1, rest1 = src:match("^(.-)//(.-)\n(.*)$")
 		local text2, comment2, rest2 = src:match("^(.-)/*(.-)*/(.*)$")
@@ -27,9 +40,16 @@ function stripComments(src)
 	return out
 end
 
+function preprocess(source)
+	for trigraph, character in pairs(trigraphs) do
+		source = source:gsub(trigraph:gsub(".", "%%%0"), character)
+	end
+	return stripComments(source)
+end
+
 local fileName = arg[1]
 if not fileName then
-	return print("usage:\n\tlua compiler.lua <file.c>")
+	return print("usage:\n\tlua cpp.lua <file.c>")
 end
 local file = io.open(fileName)
 if not file then
@@ -37,4 +57,4 @@ if not file then
 end
 
 local data = file:read("*all")
-print(stripComments(data))
+print(preprocess(data))
